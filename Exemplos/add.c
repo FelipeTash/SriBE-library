a5dfbf78a27fae7037f4c5c067035e3195eb3b1a
Felipe da Mata <felipenv01.764@gmail.com>
2025-08-21 09:06:53 -0300

    Update add.c

--- Exemplos/add.c
+++ Exemplos/add.c
@@ -1,1 +1,17 @@
-// continue
+// add e concat
+// main.c
+#include <stdio.h>
+#include <stdlib.h>
+#include "stringx.h"
+
+int main() {
+    char *nome = add("Pedro");
+    char *frase = concat(nome, " gosta de pizza");
+
+    printf("Frase: %s\n", frase);
+
+    free(nome);
+    free(frase);
+
+    return 0;
+}
