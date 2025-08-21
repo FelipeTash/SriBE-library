6a1ba07150879597e5bfd76b0eca65494d96249c
Felipe da Mata <felipenv01.764@gmail.com>
2025-08-21 09:08:20 -0300

    Add files via upload

---
+++ stribe.c
@@ -0,0 +1,49 @@
+// stringx.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "stringx.h"
+
+int is_equal(const char *a, const char *b) {
+    return strcmp(a, b) == 0;
+}
+
+char* add(const char *src) {
+    char *copy = malloc(strlen(src) + 1);
+    if (copy) strcpy(copy, src);
+    return copy;
+}
+
+char* concat(const char *a, const char *b) {
+    size_t len = strlen(a) + strlen(b) + 1;
+    char *res = malloc(len);
+    if (res) {
+        strcpy(res, a);
+        strcat(res, b);
+    }
+    return res;
+}
+
+char* uppercase(const char *src) {
+    size_t len = strlen(src);
+    char *upper = malloc(len + 1);
+    if (upper) {
+        for (size_t i = 0; i < len; i++) {
+            upper[i] = toupper(src[i]);
+        }
+        upper[len] = '\0';
+    }
+    return upper;
+}
+char* lowercase(const char *src) {
+    size_t len = strlen(src);
+    char *lower = malloc(len + 1);
+    if (lower) {
+        for (size_t i = 0; i < len; i++) {
+            lower[i] = tolower(src[i]);
+        }
+        lower[len] = '\0';
+    }
+    return lower;
+}
---
+++ stribe.h
@@ -0,0 +1,10 @@
+// stringx.h
+#ifndef STRINGX_H
+#define STRINGX_H
+
+int is_equal(const char *a, const char *b);
+char* add(const char *src);
+char* concat(const char *a, const char *b);
+char* uppercase(const char *src);
+
+#endif
