b1aa28aff66e800316d32a9ac2d848a0457f1db1
Felipe da Mata <felipenv01.764@gmail.com>
2025-08-21 09:36:36 -0300

    Update stribe.c

--- stribe.c
+++ stribe.c
@@ -1,9 +1,9 @@
-// stringx.c
+// StriBE.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include "stringx.h"
+#include "stribe.h"
 
 int is_equal(const char *a, const char *b) {
     return strcmp(a, b) == 0;
