55032073a55a0d2bdf81f3123d071a3a14f52295
Felipe da Mata <felipenv01.764@gmail.com>
2025-08-21 19:51:07 -0300

    Add files via upload

---
+++ Exemplos/lower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "stribe.h"
+
+int main() {
+    const char *original = "Hello, World!";
+    char *lowercased = lowercase(original);
+    
+    if (lowercased != NULL) {
+        printf("Original: %s\n", original);
+        printf("Lowercase: %s\n", lowercased);
+    } else {
+        printf("Memory allocation failed.\n");
+    }
+
+    // Free the allocated memory
+    free(lowercased);
+    
+    return 0;
+}
